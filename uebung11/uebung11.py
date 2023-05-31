a53bed22a5d519e3463b7e5b0ae04d549cb8b6a0
mendacium <111198852+mendacium1@users.noreply.github.com>
2023-05-31 19:34:19 +0200

    Finish uebung11
    Finish uebung11

--- uebung11/uebung11.py
+++ uebung11/uebung11.py
@@ -1,17 +1,9 @@
-import si
-
-print(si.list_primes(10,35))
-
-
 """
 2.)
 """
-def aufgabe2(number):
-    if pow(number, 3, 25) == 1 or pow(number, 3, 25) == 24:
-        return True
-    elif pow(number, 2*3, 25) == 24:
-        return True
-
-for n in range(1, 25):
-    
+# n - 1 = 2^3 * 3
+for i in range(1, 25):
+    for s in range(0,4):
+        if (pow(i, 2**s, 25)) == 1 or (pow(i, 2**s, 25)) == 24:
+            print(f"{i} ist ein Zeuge")
 
